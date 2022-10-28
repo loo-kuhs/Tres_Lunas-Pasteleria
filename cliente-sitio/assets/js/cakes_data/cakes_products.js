551fb44b0f06b328f60b157389b3dfb4e9030230
lucasbear <lucasbear.yo@outlook.com>
2022-10-28 18:39:52 -0500

    Added some cakes to the products section

---
+++ cliente-sitio/assets/js/cakes_data/cakes_products.js
@@ -0,0 +1,30 @@
+let cakesData = [
+  {
+    id: 1,
+    image: "../../img/cakes/presentation--img-min-01.jpg",
+    cake: {
+      name: "Tres Leches de Venus",
+      flavors: [
+        {
+          id: 1,
+          flavor: 'chocolate',
+        },
+        {
+          id: 2,
+          flavor: 'vainilla'
+        }
+      ],
+      fruits: [
+        {
+          id: 1,
+          fruit: 'fresa'
+        },
+        {
+          id: 2,
+          fruit: 'piña'
+        }
+      ]
+    },
+    description: 'El pastel de vainilla más delicioso que probaras.'
+  },
+];
